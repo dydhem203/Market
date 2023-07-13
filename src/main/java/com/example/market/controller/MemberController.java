1510fa5c2defa1394280e22b5e31923990e27834
김영은 <99572560+youngeun10@users.noreply.github.com>
2023-07-13 22:40:42 +0900

    회원 Controller create

---
+++ src/main/java/com/example/market/controller/MemberController.java
@@ -0,0 +1,24 @@
+package com.example.market.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequiredArgsConstructor
+@RequestMapping("/member")
+public class MemberController {
+
+    @GetMapping("/login")
+    public String loginForm() {
+        return "loginForm";
+    }
+
+    @GetMapping("/signup")
+    public String signupForm() {
+        return "signupForm";
+    }
+
+
+}
