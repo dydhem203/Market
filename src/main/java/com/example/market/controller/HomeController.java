ce48af8da255009561f83a276fb23d3c926fd415
김영은 <99572560+youngeun10@users.noreply.github.com>
2023-07-16 15:52:04 +0900

    home에서 login, signup 화면 연결, 회원가입 폼 시작!

--- src/main/java/com/example/market/controller/HomeController.java
+++ src/main/java/com/example/market/controller/HomeController.java
@@ -4,9 +4,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class MainController {
+public class HomeController {
     @GetMapping("/")
     public String home() {
-        return "main";
+        return "home";
     }
 }
